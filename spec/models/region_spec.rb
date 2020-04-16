b89347531e67d3715bcbc9c3533c32359e537410
Natashia White <whitenat@oregonstate.edu>
2020-04-16 09:39:58 -0700

    Create and pass instance tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-
+it 'exists' do
+	Region = Region.new
+end
 end
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-
+it 'exists' do
+	ResourceCategory = ResourceCategory.new
+end
 end
