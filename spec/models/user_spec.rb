7e3e8bd4da6fdd3d1c58e3eca33a47f6c78434b2
Natashia White <whitenat@oregonstate.edu>
2020-04-16 09:47:41 -0700

    create and pass user attribute tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-it 'exists' do
-	User = User.new
-end
+	it { is_expected.to respond_to(:email) }
+	it { is_expected.to respond_to(:role) }
+ 
 end
