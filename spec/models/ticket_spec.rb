81816c9afef389c61640ab6e2c3a70ab1bf02079
Natashia White <whitenat@oregonstate.edu>
2020-04-16 09:54:44 -0700

    create and pass ticket attribute tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-it 'exists' do
-	Ticket = Ticket.new
-end
+	it { is_expected.to respond_to(:name) }
+	it { is_expected.to respond_to(:phone) }
+	it { is_expected.to respond_to(:region_id) }
+	it { is_expected.to respond_to(:resource_category_id) }
 end
