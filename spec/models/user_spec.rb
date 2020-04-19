f4b77feb6d20e0eb601413893a70e34264e89216
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:38:12 -0700

    add and pass user responds to reset password token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:email) }
 	it { is_expected.to respond_to(:encrypted_password) }
+	it { is_expected.to respond_to(:reset_password_token) }
  
 end
