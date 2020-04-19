7d4af7ba945f667efc5df1a39082c1ededd5d786
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:28:32 -0700

    add and pass ticket responds to description

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Ticket, type: :model do
 	it { is_expected.to respond_to(:phone) }
 	it { is_expected.to respond_to(:region_id) }
 	it { is_expected.to respond_to(:resource_category_id) }
+	it { is_expected.to respond_to(:description) }
 end
