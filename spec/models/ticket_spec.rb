0fa1046dceada1dd1cd89a9e584de17833bcaf75
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:33:19 -0700

    add and pass ticket responds to organization id

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 	it { is_expected.to respond_to(:name) }
+	it { is_expected.to respond_to(:description) }
 	it { is_expected.to respond_to(:phone) }
+	it { is_expected.to respond_to(:organization_id) }
 	it { is_expected.to respond_to(:region_id) }
 	it { is_expected.to respond_to(:resource_category_id) }
-	it { is_expected.to respond_to(:description) }
+
 end
