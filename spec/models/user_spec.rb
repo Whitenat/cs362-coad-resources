99509fbfda2fb99478ba6898e2ac7a5f42ffacf9
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:38:59 -0700

    add and pass user responds to reset password sent at

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:email) }
 	it { is_expected.to respond_to(:encrypted_password) }
 	it { is_expected.to respond_to(:reset_password_token) }
+	it { is_expected.to respond_to(:reset_password_sent_at) }
  
 end
