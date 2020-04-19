02c4da61b89603183e66eefbba4783ee7040db07
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:34:22 -0700

    add and pass ticket responds to updated at

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Ticket, type: :model do
 	it { is_expected.to respond_to(:phone) }
 	it { is_expected.to respond_to(:organization_id) }
 	it { is_expected.to respond_to(:created_at) }
+	it { is_expected.to respond_to(:updated_at) }
 	it { is_expected.to respond_to(:region_id) }
 	it { is_expected.to respond_to(:resource_category_id) }
 
