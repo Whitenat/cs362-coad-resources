f6755f9d5557005017e7055ca37e6cdb94f0d6fa
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:39:33 -0700

    add and pass user responds to remember created at

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:encrypted_password) }
 	it { is_expected.to respond_to(:reset_password_token) }
 	it { is_expected.to respond_to(:reset_password_sent_at) }
+	it { is_expected.to respond_to(:remember_created_at) }
  
 end
