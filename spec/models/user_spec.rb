1ab18cbbdcd3730a2a951accaa0779fce1acd9ad
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:26:57 -0700

    add and pass user responds to password test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:email) }
 	it { is_expected.to respond_to(:role) }
+	it { is_expected.to respond_to(:password) }
  
 end
