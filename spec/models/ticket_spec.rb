6ec4113f4256e4614551d8d8737220579e427574
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:34:46 -0700

    add and pass ticket responds to closed

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Ticket, type: :model do
 	it { is_expected.to respond_to(:organization_id) }
 	it { is_expected.to respond_to(:created_at) }
 	it { is_expected.to respond_to(:updated_at) }
+	it { is_expected.to respond_to(:closed) }
 	it { is_expected.to respond_to(:region_id) }
 	it { is_expected.to respond_to(:resource_category_id) }
 
