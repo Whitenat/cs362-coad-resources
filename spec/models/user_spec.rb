a99f790d952ac01d6a901f729664e70aa907cbd9
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:40:30 -0700

    add and pass user responds to updated at

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:reset_password_sent_at) }
 	it { is_expected.to respond_to(:remember_created_at) }
 	it { is_expected.to respond_to(:created_at) }
+	it { is_expected.to respond_to(:updated_at) }
  
 end
