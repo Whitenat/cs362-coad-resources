5a49fad61e93d5ca17aa28c923ee4a8921ff0eb5
Natashia White <whitenat@oregonstate.edu>
2020-04-19 13:37:37 -0700

    add and pass user responds to encrypted password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 	it { is_expected.to respond_to(:email) }
-	it { is_expected.to respond_to(:role) }
-	it { is_expected.to respond_to(:password) }
+	it { is_expected.to respond_to(:encrypted_password) }
  
 end
