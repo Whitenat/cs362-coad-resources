16b629cf186bf7982b9f0d3e4fc63c1b87115aff
KristenOrue <oruek@oregonstate.edu>
2020-04-19 22:18:43 -0700

    Add tests to region_spec.rb, Have not yet tested

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
+
+
 RSpec.describe Region, type: :model do
-it 'exists' do
-	Region = Region.new
-end
+	it { is_expected.to respond_to(:name) }
+	it { is_expected.to respond_to(:created_at) }
+	it { is_expected.to respond_to(:updated_at) }
+# it 'exists' do
+# 	Region = Region.new
+# end
 end
