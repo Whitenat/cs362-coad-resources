13f75d7a8a433cb69bf18b62d13f75ef11fac840
KristenOrue <oruek@oregonstate.edu>
2020-04-19 22:32:02 -0700

    Add and pass organization test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-
-it 'exists' do
-	Organization = Organization.new
-end
-
+	it { is_expected.to respond_to(:name) }
+	it { is_expected.to respond_to(:status) }
+	it { is_expected.to respond_to(:phone) }
+	it { is_expected.to respond_to(:email) }
+	it { is_expected.to respond_to(:description) }
+	it { is_expected.to respond_to(:rejection_reason) }
+	it { is_expected.to respond_to(:created_at) }
+	it { is_expected.to respond_to(:updated_at) }
+	it { is_expected.to respond_to(:liability_insurance) }
+	it { is_expected.to respond_to(:primary_name) }
+	it { is_expected.to respond_to(:secondary_name) }
+	it { is_expected.to respond_to(:secondary_phone) }
+	it { is_expected.to respond_to(:title) }
+	it { is_expected.to respond_to(:transportation) }
+	it { is_expected.to respond_to(:email) }
+	it { is_expected.to respond_to(:name) }
+# it 'exists' do
+# 	Organization = Organization.new
+# end
 end
