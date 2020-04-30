1cd637c63f2a7c53dbe178c57f7c240b4ff8db82
KristenOrue <oruek@oregonstate.edu>
2020-04-30 09:14:06 -0700

    Add Relationship tests to region_spec.rb

--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
     factory :region do
         name { ' Fake' }
-
+        tickets { [] }
         trait :unspecified do
             name { 'Unspecified' }
         end
-
     end
-
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
-
-
 RSpec.describe Region, type: :model do
 let(:region) {FactoryBot.build(:region) }
 let(:unspecified_region) {FactoryBot.build(:region) }
@@ -9,7 +7,13 @@ let(:unspecified_region) {FactoryBot.build(:region) }
 	it { is_expected.to respond_to(:name) }
 	it { is_expected.to respond_to(:created_at) }
 	it { is_expected.to respond_to(:updated_at) }
-# it 'exists' do
-# 	Region = Region.new
-# end
 end
+
+describe "relationships" do
+	it {is_expected.to have_many(:tickets)}
+end
+
+describe "validations" do
+	it { is_expected.to validate_presence_of(:name) }
+end
+	
