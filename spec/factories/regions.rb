2a779c66e6738b8f8eda400b3a9f2349964ff88b
KristenOrue <oruek@oregonstate.edu>
2020-04-30 08:54:06 -0700

    Add a Factory Bot for Region

---
+++ spec/factories/regions.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory :region do
+        name { ' Fake' }
+
+        trait :unspecified do
+            name { 'Unspecified' }
+        end
+
+    end
+
+end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 
 
 RSpec.describe Region, type: :model do
+let(:region) {FactoryBot.build(:region) }
+let(:unspecified_region) {FactoryBot.build(:region) }
+
 	it { is_expected.to respond_to(:name) }
 	it { is_expected.to respond_to(:created_at) }
 	it { is_expected.to respond_to(:updated_at) }
