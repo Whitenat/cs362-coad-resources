4d46b156f4b43243e286d406c91b9371942259cc
KristenOrue <oruek@oregonstate.edu>
2020-05-04 22:45:26 -0700

    Fix region_spec.rb unspecified and spec/factories/regions.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-	# let(:region) {FactoryBot.build(:region) }
-	# let(:unspecified_region) {FactoryBot.build(:region) }
+	let(:region) {FactoryBot.build(:region) }
+	let(:unspecified_region) {FactoryBot.build(:region) }
 
 	describe 'properties' do
 		it { is_expected.to respond_to(:name) }
@@ -22,7 +22,28 @@ RSpec.describe Region, type: :model do
 		it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 	end
 
+	# describe "#to_s" do
+	# 	it "has a string representation that is the name" do
+	# 	expect(region.to_s).to eq('FAKE')
+	# 	end
+	# end
+
+	describe ": :unspecified" do	
+		it "creates a new Unspeciified region when one does not exist" do
+			expect(Region.where(name: 'Unspecified')).to be_empty
+			expect{ Region.unspecified }.to change { Region.count }
+		end
+		# it "does not create a new Unspecified region when one already exists" do
+		# 	FactoryBot.create(:region, :unspecified)
+		# 	expect{ Region.unspecified }.to_not change { Region.count }
+		# end
+		it "does not create a new Unspecified region when one already has one" do
+			expect(Region.unspecified.name).to eq('Unspecified')
+		end
+	end
 end
+		
+
 
 
 	
