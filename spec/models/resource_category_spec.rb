251df94e15463006046d3e357ceaf1b7b0a30748
Natashia White <whitenat@oregonstate.edu>
2020-05-04 18:03:15 -0700

    add relationship validation to resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe ResourceCategory, type: :model do
 	end
 
 	describe 'relationships' do
-		it { is_expected.to belong_to(:organizations) }
+		it { is_expected.to have_and_belong_to_many(:organizations) }
 	end
 
 	describe 'validations' do
