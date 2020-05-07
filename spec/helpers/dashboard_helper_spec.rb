5e683b50042dbf2a8a8da98786e030e5a3f2a046
Natashia White <whitenat@oregonstate.edu>
2020-05-07 15:27:08 -0700

    get dashboard helper 100% under test

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -15,15 +15,22 @@ RSpec.describe DashboardHelper, type: :helper do
 	describe DashboardHelper do
 
 	  describe "dashboard_for" do
-	    # it "sends admin user to admin dashboard" do
-	    #   expect(helper.dashboard_for(User.where(role: "admin"))).to eq("admin_dashboard")
-	    # end
-	    # it "sends submitted organizations to submitted dash" do
-	    #   expect(helper.dashboard_for()).to eq("organization_submitted_dashboard")
-	    # end
-	    # it "sends approved organizations to approved dash" do
-	    #   expect(helper.dashboard_for(Organization.new.approve())).to eq("organization_approved_dashboard")
-	    # end
+	    it "sends admin user to admin dashboard" do
+	    	adminUser = User.new(role: "admin") 
+	      expect(helper.dashboard_for(adminUser)).to eq("admin_dashboard")
+	    end
+
+	    it "sends submitted organizations to submitted dash" do
+	    	submittedOrganization = User.new(role: "organization", organization: Organization.new) 
+	      expect(helper.dashboard_for(submittedOrganization)).to eq("organization_submitted_dashboard")
+	    end
+
+	    it "sends approved organizations to approved dash" do
+	    	organization = Organization.new(status: "approved")
+	    	approvedOrganization = User.new(role: "organization", organization: organization)
+	      expect(helper.dashboard_for(approvedOrganization)).to eq("organization_approved_dashboard")
+	    end
+
 	    it "sends unknown users to default dash" do
 	      expect(helper.dashboard_for(User.new)).to eq("create_application_dashboard")
 	    end
