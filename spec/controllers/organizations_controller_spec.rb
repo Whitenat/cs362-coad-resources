9098b838ba95a3b9d3d7a7d1870a1c54468e8810
KristenOrue <oruek@oregonstate.edu>
2020-05-21 09:55:04 -0700

    Add method tests to OrganizationsController.rb for authenticate user

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,12 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
-    describe OrganizationsController do
-        describe "index" do
-            it "Orders Organization by name" do
-                
-            end
+
+    context 'As a public user' do
+        describe 'GET #index' do
+            specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+        end
+
+        specify "GET #show" do
+            expect(get(:show, params: {id: 'FAKE'})).to redirect_to(new_user_session_path)
+        end
+
+        specify "POST #new" do
+            expect(post(:new)).to redirect_to(new_user_session_path )
+        end
+
+        specify "POST #create" do
+            expect(post(:create, params: {region: attributes_for(:region)})).to redirect_to(new_user_session_path)
+        end
+
+        specify "PUT #update" do
+            expect(put(:update, params: {id: 'FAKE', region: attributes_for(:region)})).to redirect_to(new_user_session_path)
         end
     end
-
 end
+
+# context 'Organization user' do
+#     describe 'POST #create' do 
+#     let(:user) do
+#         User.create(email: 'fake@fake.com',
+#             password: 'password',
+#             password_confirmation: 'password',
+#             role: 'organization')
+#     end
+#     before do
+#         user.confirm
+#         sign_in(user)
+#     end
+#     it 'succeeds' do
+#         expect(
+#         post(
+#             :create,
+#             params: {
+#                 organization: {name: 'FAKE'},
+#                 email: 'fake@fake.com',
+#                 phone: '13045739394',
+#                 primary_name: 'FAKE primary name',
+#                 secondary_name: 'FAKE secondary name',
+#                 secondary_phone: '13037845463'
+#     },
+# }
+#     )
+# ).to redirect_to(organization_application_submitted_path)
+# end
+#         }
+#     )
+# )
+# end
