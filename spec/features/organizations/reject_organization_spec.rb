fd4da240f52ba33d502a3dac80daec54d40b4089
Natashia White <whitenat@oregonstate.edu>
2020-05-26 20:01:58 -0700

    add reject organization feature test

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+		before(:each) do
+        @organization = FactoryBot.create(:organization)
+        @user = FactoryBot.create(:user, :admin)
+        log_in_as(@user)
+    end
 
+	it 'displays rejected message' do
+		visit organizations_path
+		click_on 'Pending'
+		click_on @organization.name
+		fill_in 'Rejection Reason', with: 'Rejected'
+		click_on 'Reject' 
+		expect(page).to have_content('Organization '+ @organization.name + ' has been rejected.')
+	end
 end
