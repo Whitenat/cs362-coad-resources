a0a6c835fe966c78aa4d859c14e81eff1b7c1606
Natashia White <whitenat@oregonstate.edu>
2020-05-26 20:33:26 -0700

    add sends new organization confirmation email

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
 
+	it 'sends confirmation email' do
+		visit signup_path
+		fill_in 'Email', with: 'fake@mail.com'
+		fill_in 'Password', with: 'Password'
+		fill_in 'Password confirmation', with: 'Password'
+		click_button 'Sign up'
+		expect(page).to have_content('A message with a confirmation link has been sent to your email address')
+
+	end
 end
