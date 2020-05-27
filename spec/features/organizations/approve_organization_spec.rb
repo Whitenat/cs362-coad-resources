6f492d0bf436d241053d73e3c60aebd19f1bfefb
Natashia White <whitenat@oregonstate.edu>
2020-05-26 19:57:12 -0700

    add first feature testgit add .

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+	before(:each) do
+        @organization = FactoryBot.create(:organization)
+        @user = FactoryBot.create(:user, :admin)
+        log_in_as(@user)
+    end
+
+	it 'displays approved message' do
+		visit organizations_path
+		click_on 'Pending'
+		click_on @organization.name
+		click_on 'Approve' 
+		expect(page).to have_content('Organization '+ @organization.name + ' has been approved.')
+	end
+
 end
--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+	before :each do
+		@region = Region.create(name: "Fake Region")
+		@resource_category = ResourceCategory.create(name: "Fake Catery")
+	end
+
+	it 'displays a success message' do
+		visit new_ticket_path
+		fill_in 'Full Name', with: 'Fake Name'
+		fill_in 'Phone Number', with: '3036531234'
+		select @region.name, from: 'Region'
+		select @resource_category.name, from: 'Resource Category'
+		fill_in 'Description', with: 'Fake'
+		click_on 'Send this help request'
+		expect(page).to have_content('Ticket Submitted')
+	end
+
+	it 'displays a error message when a phone is invalid' do
+		visit new_ticket_path
+		fill_in 'Full Name', with: 'Fake Name'
+		fill_in 'Phone Number', with: 'INVALID'
+		select @region.name, from: 'Region'
+		select @resource_category.name, from: 'Resource Category'
+		fill_in 'Description', with: 'Fake'
+		click_on 'Send this help request'
+		expect(page).to have_content("error")
+	end
 end
