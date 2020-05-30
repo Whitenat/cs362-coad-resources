3502fb6a8bd4f5e3889a36b37039424e8e94ac2c
Natashia White <whitenat@oregonstate.edu>
2020-05-30 16:08:42 -0700

    add admin create region spec

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+	# before(:each) do
+ #        @user = FactoryBot.create(:user, :organization_member)
+ #        @admin_user = FactoryBot.create(:user, :admin)
+ #        log_in_as(@user)
+ #    end
+
+ #    it 'creates a new application and then updates organization' do
+ #    	visit dashboard_path
+ #    	click_on 'Create Application'
+ #    	choose('organization_agreement_one_true')
+ #    	choose('organization_agreement_two_true')
+ #    	choose('organization_agreement_three_true')
+ #    	choose('organization_agreement_four_true')
+ #    	choose('organization_agreement_five_true')
+ #    	choose('organization_agreement_six_true')
+ #    	choose('organization_agreement_seven_true')
+ #    	choose('organization_agreement_eight_true')
+ #    	fill_in 'What is your name? (Last, First)', with: 'last, first'
+ #    	fill_in 'Organization Name', with: 'fake organization'
+ #    	fill_in 'What is your title? (if applicable)', with: 'Fake Name1'
+ #    	fill_in 'What is your direct phone number? Cell phone is best.', with: '1112223333'
+ #    	fill_in 'Who may we contact regarding your organization\'s application if we are unable to reach you?', with: 'Fake Name2'
+ #    	fill_in 'What is a good secondary phone number we may reach your organization at?', with: '3332221111'
+ #    	fill_in 'What is your Organization\'s email?', with: 'email@domain.com'
+ #    	fill_in 'organization_description', with: 'description'
+ #    	choose('organization_transportation_yes')
+ #    	click_button 'Apply'
+ #    	expect(page).to have_content('Application Submitted')
+ #    	click_on 'Log out'
+ #    	log_in_as(@user)
+ #    	click_button 'Edit Organization'
+ #    	expect(page).to have_content('Edit Organization')
+    	
+ #    end
 
 end
--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+	before(:each) do
+        @user = FactoryBot.create(:user, :admin)
+        log_in_as(@user)
+    end
+
+    context 'as an admin' do
+    	it 'creates a region' do
+    		click_link 'Regions'
+            click_on 'Add Region'
+            expect(page).to have_content('Add a region')
+            fill_in 'Name', with: 'Fake Region'
+            click_button 'Add Region'
+            expect(page).to have_content('Region successfully created.')
+    	end
+    end
 
 end
