74b19fd80075ab896d3e0ac485b54fc64bcda247
Natashia White <whitenat@oregonstate.edu>
2020-05-31 11:04:16 -0700

    complete resource categories controller

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -3,68 +3,68 @@ require 'rails_helper'
 RSpec.describe OrganizationsController, type: :controller do
 
     context 'As a public user' do
-        describe 'GET #index' do
-            specify { expect(get(:index)).to redirect_to(new_user_session_path) }
-        end
+      describe 'GET #index' do
+        specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+      end
 
-        specify "GET #show" do
-            expect(get(:show, params: {id: 'FAKE'})).to redirect_to(new_user_session_path)
-        end
+      specify "GET #show" do
+        expect(get(:show, params: {id: 'FAKE'})).to redirect_to(new_user_session_path)
+      end
 
-        specify "GET #new" do
-            expect(get(:new)).to redirect_to(new_user_session_path )
-        end
+      specify "GET #new" do
+        expect(get(:new)).to redirect_to(new_user_session_path )
+      end
 
-        specify "POST #create" do
-            expect(post(:create, params: {organization: attributes_for(:organization)})).to redirect_to(new_user_session_path)
-        end
+      specify "POST #create" do
+        expect(post(:create, params: {organization: attributes_for(:organization)})).to redirect_to(new_user_session_path)
+      end
 
-        specify "PUT #update" do
-            expect(put(:update, params: {id: 'FAKE', organization: attributes_for(:organization)})).to redirect_to(new_user_session_path)
-        end
+      specify "PUT #update" do
+        expect(put(:update, params: {id: 'FAKE', organization: attributes_for(:organization)})).to redirect_to(new_user_session_path)
+      end
 
-        specify "POST #approve" do
-            expect(post(:approve, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)
-        end
+      specify "POST #approve" do
+        expect(post(:approve, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)
+      end
 
-        specify "POST #reject" do
-            expect(post(:reject, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)
-        end
+      specify "POST #reject" do
+        expect(post(:reject, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)
+      end
     end
 
     context 'As an Organization User' do
         let(:user) { create(:user, :organization_member)}
         let(:admin_user) {create(:user, :admin)}
         before do   
-            sign_in(user)
+          sign_in(user)
         end
 
         specify 'GET #index' do
-            expect(get(:index)).to be_successful 
+          expect(get(:index)).to be_successful 
         end
 
         specify "GET #new" do
-            expect(get(:new)).to be_successful
+          expect(get(:new)).to be_successful
         end
 
         specify "POST #create" do
-            sign_in(admin_user)
-            expect(post(:create, params: {organization: attributes_for(:organization)})).to redirect_to("/dashboard")
+          sign_in(admin_user)
+          expect(post(:create, params: {organization: attributes_for(:organization)})).to redirect_to("/dashboard")
         end
 
         specify "PUT #update" do
-            organization = create(:organization)
-            expect(put(:update, params:{id: organization.id, organization: attributes_for(:organization)})).to redirect_to("/dashboard")
+          organization = create(:organization)
+          expect(put(:update, params:{id: organization.id, organization: attributes_for(:organization)})).to redirect_to("/dashboard")
         end
 
         specify "POST #approve" do
-            organization = create(:organization)
-            expect(post(:approve, params:{id: organization.id})).to redirect_to("/dashboard")
+          organization = create(:organization)
+          expect(post(:approve, params:{id: organization.id})).to redirect_to("/dashboard")
         end
 
         specify "POST #reject" do
-            organization = create(:organization)
-            expect(post(:reject, params:{id: organization.id})).to redirect_to("/dashboard")
+          organization = create(:organization)
+          expect(post(:reject, params:{id: organization.id})).to redirect_to("/dashboard")
         end
 
     end
@@ -72,34 +72,34 @@ RSpec.describe OrganizationsController, type: :controller do
     context 'As an Admin User' do
         let(:user) {create(:user, :admin)}
         before do   
-            sign_in(user)
+          sign_in(user)
         end
 
         specify 'GET #index' do
-            expect(get(:index)).to be_successful 
+          expect(get(:index)).to be_successful 
         end
 
         specify "GET #new" do
-            expect(get(:new)).to redirect_to("/dashboard")
+          expect(get(:new)).to redirect_to("/dashboard")
         end
 
         specify "POST #create" do
-            expect(post(:create, params: {organization: attributes_for(:organization)})).to redirect_to("/dashboard")
+          expect(post(:create, params: {organization: attributes_for(:organization)})).to redirect_to("/dashboard")
         end
 
         specify "PUT #update" do
-            organization = create(:organization)
-            expect(put(:update, params:{id: organization.id, organization: attributes_for(:organization)})).to redirect_to("/dashboard")
+          organization = create(:organization)
+          expect(put(:update, params:{id: organization.id, organization: attributes_for(:organization)})).to redirect_to("/dashboard")
         end
 
         specify "POST #approve" do
-            organization = create(:organization)
-            expect(post(:approve, params:{id: organization.id})).to redirect_to("/organizations")
+          organization = create(:organization)
+          expect(post(:approve, params:{id: organization.id})).to redirect_to("/organizations")
         end
 
         specify "POST #reject" do
-            organization = create(:organization)
-            expect(post(:reject, params: { id: organization.id, organization: {rejection_reason: 'FAKE'} } )).to redirect_to("/organizations")
+          organization = create(:organization)
+          expect(post(:reject, params: { id: organization.id, organization: {rejection_reason: 'FAKE'} } )).to redirect_to("/organizations")
         end
     end
 
--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,110 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+	context 'As a public user' do
+		describe 'GET #index' do
+      specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+    end
 
+    describe 'GET #new' do
+      specify { expect(get(:new)).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'POST #create' do
+    	specify { expect(post(:create)).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'PUT #update' do
+    	specify { expect(put(:update, params: {id: 'FAKE'})).to redirect_to(new_user_session_path)}
+    end
+
+    describe 'PATCH #activate' do
+    	specify { expect(patch(:activate, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)}
+    end
+
+    describe 'PATCH #deactivate' do
+    	specify { expect(patch(:deactivate, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)}
+    end
+
+    describe 'DELETE #destroy' do
+    	specify { expect(delete(:destroy, params:{id: 'FAKE'})).to redirect_to(new_user_session_path)}
+    end
+
+	end
+
+	context 'As an organization user' do
+    let(:user) { create(:user)}
+    before do   
+        sign_in(user)
+    end
+
+		describe 'GET #index' do
+      specify { expect(get(:index)).to redirect_to(dashboard_path) }
+    end
+
+    describe 'GET #new' do
+      specify { expect(get(:new)).to redirect_to(dashboard_path) }
+    end
+
+    describe 'POST #create' do
+    	specify { expect(post(:create)).to redirect_to(dashboard_path) }
+    end
+
+    describe 'PUT #update' do
+    	specify { expect(put(:update, params: {id: 'FAKE'})).to redirect_to(dashboard_path)}
+    end
+
+    describe 'PATCH #activate' do
+    	specify { expect(patch(:activate, params:{id: 'FAKE'})).to redirect_to(dashboard_path)}
+    end
+
+    describe 'PATCH #deactivate' do
+    	specify { expect(patch(:deactivate, params:{id: 'FAKE'})).to redirect_to(dashboard_path)}
+    end
+
+    describe 'DELETE #destroy' do
+    	specify { expect(delete(:destroy, params:{id: 'FAKE'})).to redirect_to(dashboard_path)}
+    end
+
+	end
+
+	context 'As an admin user' do
+		let(:user) { create(:user, :admin)}
+    before do   
+        sign_in(user)
+    end
+
+		describe 'GET #index' do
+      specify { expect(get(:index)).to be_successful }
+    end
+
+    describe 'GET #new' do
+      specify { expect(get(:new)).to be_successful }
+    end
+
+    describe 'POST #create' do
+    	specify { expect(post(:create, params: {resource_category: attributes_for(:resource_category)})).to redirect_to(resource_categories_path) }
+    end
+
+    specify 'PUT #update' do
+    	resource_category = create(:resource_category)
+    	expect(put(:update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})).to redirect_to(resource_category_path(resource_category))
+    end
+
+    specify 'PATCH #activate' do
+    	resource_category = create(:resource_category)
+      expect(patch(:activate, params:{id: resource_category.id})).to redirect_to(resource_category_path(resource_category))
+    end
+
+    specify 'PATCH #deactivate' do
+    	resource_category = create(:resource_category)
+      expect(patch(:deactivate, params:{id: resource_category.id})).to redirect_to(resource_category_path(resource_category))
+    end
+
+    specify 'DELETE #destroy' do
+    	resource_category = create(:resource_category)
+    	expect(delete(:destroy, params:{id: resource_category.id})).to redirect_to(resource_categories_path)
+    end
+
+	end
 end
