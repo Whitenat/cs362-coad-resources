a3166c8a29ee66998e6b2ef0e915915dd434c636
Natashia White <whitenat@oregonstate.edu>
2020-05-31 11:51:26 -0700

    add as public user tests to tickets controller

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,52 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+	context 'As a public user' do
+		let(:user) { create(:user)}
+
+		specify 'GET #new' do
+      expect(get(:new)).to be_successful
+    end
+
+    specify 'POST #create' do
+      expect(post(:create, params:{ticket: attributes_for(:ticket)})).to be_successful
+    end
+
+    specify 'GET #show' do
+    	ticket = create(:ticket)
+      sign_in(user)
+    	expect(get(:show, params: {id: ticket.id})).to redirect_to(dashboard_path)
+    end
+
+    specify 'POST #capture' do
+    	ticket = create(:ticket)
+      sign_in(user)
+    	expect(post(:capture, params: {id: ticket.id})).to redirect_to(dashboard_path)
+    end
+
+    specify 'POST #release' do
+    	ticket = create(:ticket)
+      sign_in(user)
+    	expect(post(:release, params: {id: ticket.id})).to redirect_to(dashboard_path)
+    end
+
+    specify 'PATCH #close' do
+    	ticket = create(:ticket)
+      sign_in(user)
+    	expect(patch(:close, params: {id: ticket.id})).to redirect_to(dashboard_path)
+    end
+
+    specify 'DELETE #destroy' do
+    	expect(delete(:destroy, params: {id: 'FAKE'})).to redirect_to(dashboard_path)
+    end
+	end
+
+	context 'As an organization user' do
+
+	end
+
+	context 'As an admin user' do
+
+	end
+
 end
